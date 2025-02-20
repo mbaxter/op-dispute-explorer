aeee13d94732dbe689e97f42fca07b2046802211
mbaxter <meredith@oplabs.co>
2025-02-19 22:14:48 -0500

    Extract copy button component

---
+++ src/components/CopyButton.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import CopyIcon from './CopyIcon.svelte';
+	import Tooltip from './Tooltip.svelte';
+
+	export let value: string;
+	
+	let copyTooltipText = 'copy';
+
+	async function copyToClipboard() {
+		await navigator.clipboard.writeText(value);
+		copyTooltipText = 'copied';
+		setTimeout(() => {
+			copyTooltipText = 'copy';
+		}, 2000);
+	}
+</script>
+
+<button type="button" class="copy-button" on:click={copyToClipboard}>
+	<Tooltip>
+		<div class="copy-icon">
+			<CopyIcon />
+		</div>
+		<svelte:fragment slot="content">{copyTooltipText}</svelte:fragment>
+	</Tooltip>
+</button>
+
+<style>
+	.copy-button {
+		background: none;
+		border: none;
+		padding: 0;
+		cursor: pointer;
+		display: flex;
+		align-items: center;
+	}
+
+	.copy-icon {
+		flex-shrink: 0;
+	}
+</style> 
