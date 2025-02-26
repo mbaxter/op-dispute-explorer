33991cbc90265750aa7babd6ab3bc8fe2f2ff044
mbaxter <meredith@oplabs.co>
2025-02-25 22:46:10 -0500

    Update dependencies, fix tests

--- e2e/home.test.ts
+++ e2e/home.test.ts
@@ -8,7 +8,7 @@ test('home page has expected elements', async ({ page }) => {
 
 	// Check network dropdown exists
 	await expect(page.locator('#network-select')).toBeVisible();
-	await expect(page.locator('label[for="network-select"]')).toHaveText('Choose a Network:');
+	await expect(page.locator('label[for="network-select"]')).toHaveText('Network:');
 
 	// Check connection message
 	await expect(page.locator('h1.text-gray-400')).toHaveText('Please connect to a network by selecting a network from the dropdown in the top right');
